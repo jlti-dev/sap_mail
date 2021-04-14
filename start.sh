11389a3e17030d9bd05a29804664d2eba91b486f
LarsTi <lars.tilsner@googlemail.com>
2021-04-14 20:47:38 +0200

    update

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ -z "${GATEWAY}" ]]; then
+	echo "No Gateway found, will not change routing"
+else
+	echo "changing default gateway for private subnets to ${GATEWAY}"
+
+	ip route add 10.0.0.0/8 via ${GATEWAY}
+	ip route add 172.16.0.0/12 via ${GATEWAY}
+	ip route add 192.168.0.0/16 via ${GATEWAY}
+	echo "changed routes for 10.0.0.0/8, 172.16.0.0/12 and 192.168.0.0/16"
+	echo "public nets are still available"
+fi
+/app/main
